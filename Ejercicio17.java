69523cbad3d3f7d94fbd6d407f8e087b93ad997e
Pau <misscotters@gmail.com>
2023-09-29 16:56:56 +0200

    Se ha solucionado el problema del consumo siendo menor al real

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -34,7 +34,7 @@ public class Ejercicio17{
         System.out.println("¿Cuantos kilometros marcó el cuentakilometros al final?");
         numkm = Double.parseDouble(lector.nextLine());
 
-        consumo2 = (ltotal * 100) / (numkm - numkm1);
+        consumo2 = ((ltotal * 100)*2 )/ (numkm - numkm1);
         //se hace el consumo total de los viajes 2 y 3
         consumomedio = (consumo1 + consumo2) / 2;
         //sacamos el consumo medio en litros por cada 100 km
